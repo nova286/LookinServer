c335f2d6712f36c75525a1574054ff3351282798
mlch911 <mlch911@mluoc.tech>
2023-07-27 12:42:18 +0800

    Fix Bug: Wireless Connection

--- Src/Main/Server/Connection/LKS_ConnectionManager.m
+++ Src/Main/Server/Connection/LKS_ConnectionManager.m
@@ -112,6 +112,9 @@ NSString *const LKS_ConnectionDidEndNotificationName = @"LKS_ConnectionDidEndNot
 		// 授权状态变更回调
 		self.wirelessChannel.authStateChangedBlock = ^(ECOChannelDeviceInfo *device, ECOAuthorizeResponseType authState) {
 			NSLog(@"🚀 Lookin authStateChangedBlock device:%@ authState:%ld", device, authState);
+			if (authState == ECOAuthorizeResponseType_AllowAlways) {
+				weakSelf.wirelessDevice = device;
+			}
 		};
 		// 请求授权状态认证回调
 		self.wirelessChannel.requestAuthBlock = ^(ECOChannelDeviceInfo *device, ECOAuthorizeResponseType authState) {
